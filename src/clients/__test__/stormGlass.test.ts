efb1b761d88c647568b06aea47854b0d1b64c389
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-02-07 19:48:06 -0300

    api client data normalization

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,7 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
-import stormGlassNormalizedResponse3HoursFixture from '@test/fixtures/stormglass_normilized_response_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normilized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -10,10 +10,12 @@ describe('StormGlass client', () => {
     const lat = -33.792627;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual(stormGlassNormalizedResponse3HoursFixture);
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
 });
