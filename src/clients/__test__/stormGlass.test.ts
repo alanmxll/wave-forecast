e0318ffa69a3cdd04ecdc5bd9671e613ed4cfd57
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-02-09 22:46:28 -0300

    test: should get an StormGlassResponseError when the StormGlass service responds with error

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -51,4 +51,22 @@ describe('StormGlass client', () => {
       'Unexpected error when trying to comunicate to StormGlass: Network Error'
     );
   });
+
+  it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockRejectedValue({
+      response: {
+        status: 429,
+        data: { errors: ['Rate Limit reached'] },
+      },
+    });
+
+    const stormGlass = new StormGlass(mockedAxios);
+
+    await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
+      'Unexpected error returned by the StormGlass service: Error: {"errors":["Rate Limit reached"]} Code: 429'
+    );
+  });
 });
