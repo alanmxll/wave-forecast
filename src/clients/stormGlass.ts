7a3343dacc648d3f78b3f479a4c3db669c756b85
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-02-09 22:22:03 -0300

    test: should get a generic error from StormGlass service when the request fail before reaching the service

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -37,15 +37,21 @@ export class StormGlass {
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
-    const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
-      {
-        headers: {
-          Authorization: 'fake-token',
-        },
-      }
-    );
-    return this.normalizeResponse(response.data);
+    try {
+      const response = await this.request.get<StormGlassForecastResponse>(
+        `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
+        {
+          headers: {
+            Authorization: 'fake-token',
+          },
+        }
+      );
+      return this.normalizeResponse(response.data);
+    } catch (err) {
+      throw new Error(
+        `Unexpected error when trying to comunicate to StormGlass: ${err.message}`
+      );
+    }
   }
 
   private normalizeResponse(
