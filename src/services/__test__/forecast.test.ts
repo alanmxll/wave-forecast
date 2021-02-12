9d932717da273fda5f4e050243331745e449741f
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-02-12 19:25:28 -0300

    in this test a empty list mtst be returned when beaches array is empty

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -85,4 +85,10 @@ describe('Forecast Service', () => {
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
     expect(beachesWithRating).toEqual(expectedResponse);
   });
+
+  it('should return a empty list when the beaches array is empty', async () => {
+    const forecast = new Forecast();
+    const response = await forecast.processForecastForBeaches([]);
+    expect(response).toEqual([]);
+  });
 });
