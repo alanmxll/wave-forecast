4b6fe127821026d5d28a0915469ef951ae9b21ae
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-02-12 16:24:08 -0300

    grouping forecasts by time

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -15,6 +15,11 @@ export interface Beach {
   user: string;
 }
 
+export interface TimeForecast {
+  time: string;
+  forecast: Array<BeachForecast>;
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
 export class Forecast {
@@ -22,7 +27,7 @@ export class Forecast {
 
   public async processForecastForBeaches(
     beaches: Array<Beach>
-  ): Promise<Array<BeachForecast>> {
+  ): Promise<Array<TimeForecast>> {
     const pointsWithCorrectSources: Array<BeachForecast> = [];
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
@@ -39,6 +44,24 @@ export class Forecast {
       }));
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
-    return pointsWithCorrectSources;
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(
+    forecast: Array<BeachForecast>
+  ): Array<TimeForecast> {
+    const forecastByTime: Array<TimeForecast> = [];
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+    return forecastByTime;
   }
 }
