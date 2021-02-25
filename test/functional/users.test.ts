5a03471dde47491ce36cccd6713bc038736f1039
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-02-24 21:53:00 -0300

    added: created a type error to return from user tests

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -16,5 +16,19 @@ describe('Users functions tests', () => {
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newUser));
     });
+
+    it('Should return 400 when there is a validation error', async () => {
+      const newUser = {
+        email: 'john@mail.com',
+        password: '1234',
+      };
+      const response = await global.testRequest.post('/users').send(newUser);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        code: 422,
+        error: 'User validation failed: name: Path `name` is required.',
+      });
+    });
   });
 });
