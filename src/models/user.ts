187d892ed0d9b6f5f3d55a88c19f8980207523b6
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-03-05 21:26:41 -0300

    AuthService, brcrypt and Jest Matchers

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
+import AuthService from '@src/services/auth';
 import mongoose, { Document, Model } from 'mongoose';
-
 export interface User {
   _id?: string;
   name: string;
@@ -42,5 +42,18 @@ schema.path('email').validate(
   'already exists in the database.',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (error) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
