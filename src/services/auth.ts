8c0affb62c886fd4acdb41d744b340b665c710d0
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-03-19 22:54:55 -0300

    chore: add express auth middleware and module augmentation

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,11 @@
+import { User } from '@src/models/user';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import config from 'config';
+import jwt from 'jsonwebtoken';
+
+export interface DecodedUser extends Omit<User, '_id'> {
+  id: string;
+}
 
 export default class AuthService {
   public static async hashPassword(
@@ -22,5 +27,9 @@ export default class AuthService {
     return jwt.sign(payload, config.get('App.auth.key'), {
       expiresIn: config.get('App.auth.tokenExpiresIn'),
     });
+  }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, config.get('App.auth.key')) as DecodedUser;
   }
 }
